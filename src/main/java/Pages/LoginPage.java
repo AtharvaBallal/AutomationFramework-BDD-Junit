9c6a9685e0adb8f9e25b0943d55e21b84eb69d26
MR.ROBOT <ballalatharvacr7@gmail.com>
2024-12-18 21:40:12 +0530

    Initial commit

---
+++ src/main/java/Pages/LoginPage.java
@@ -0,0 +1,19 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void page(){
+
+    }
+
+
+
+}
