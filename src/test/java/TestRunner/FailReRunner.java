4c52dcc17c6dd91355eb7ccf8320f863dccd2fb4
MR.ROBOT <ballalatharvacr7@gmail.com>
2024-12-19 20:46:58 +0530

    Added ReRunner functionality.

--- src/test/java/TestRunner/FailReRunner.java
+++ src/test/java/TestRunner/FailReRunner.java
@@ -7,14 +7,14 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        features = "src/test/resources/Features",
+        features = "@src/test/resources/RerunLog/Rerun.txt",
         glue = {"Stepdefinations", "AppHooks"},
         plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "timeline:src/test/resources/Reports/test-output-thread/",
                 "rerun:src/test/resources/RerunLog/Rerun.txt"
-        },
-        tags = "@registeruser"
+        }
+
 )
-public class RunCkues {
+public class FailReRunner {
 
 }
